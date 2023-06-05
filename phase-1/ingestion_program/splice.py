8cc177bf2168ec8f785114884559d658bdd6ca94
spencrr <23708360+spencrr@users.noreply.github.com>
2023-06-04 22:00:01 -0700

    Update splice.py

--- phase-1/ingestion_program/splice.py
+++ phase-1/ingestion_program/splice.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+
+import numpy as np
 from dataloader import AutoMLCupDataloader
-from datasets import load_dataset, DatasetDict
+from datasets import DatasetDict, load_dataset
+from sklearn.preprocessing import OneHotEncoder
 
 
 class SpliceDataloader(AutoMLCupDataloader):
@@ -19,9 +22,44 @@ class SpliceDataloader(AutoMLCupDataloader):
         if cache_directory.exists():
             self.dataset = DatasetDict.load_from_disk(cache_directory)
         else:
+            # character: meaning
+            # D:  A or G or T
+            # N:  A or G or C or T
+            # S:  C or G
+            # R:  A or G
+            label_encoder = OneHotEncoder().fit([["N"], ["EI"], ["IE"]])
+            character_classes = {
+                "A": ["A", "D", "N", "R"],
+                "C": ["C", "N", "S"],
+                "G": ["G", "D", "N", "S", "R"],
+                "T": ["T", "D", "N"],
+                "D": ["D"],
+                "N": ["N"],
+                "S": ["S"],
+                "R": ["R"],
+            }
+            character_encoder = {
+                c: i for i, c in enumerate(["A", "C", "G", "T", "D", "N", "S", "R"])
+            }
 
             def positions_to_vec(example):
-                example["input"] = list(example[f"position_{i}"] for i in range(60))
+                example["input"] = np.zeros((len(character_encoder) * 60,))
+
+                index = 0
+                for i in range(60):
+                    characters = example[f"position_{i}"]
+                    if characters == "nan":
+                        continue
+                    for character in characters:
+                        all_character_classes = character_classes[character]
+                        for character_class in all_character_classes:
+                            example["input"][
+                                index * len(character_encoder)
+                                + character_encoder[character_class]
+                            ] += 1
+                        index += 1
+                example["label"] = example["class"]
+
                 return example
 
             train_val_test_dataset = (
@@ -34,7 +72,7 @@ class SpliceDataloader(AutoMLCupDataloader):
                     positions_to_vec,
                     remove_columns=list(f"position_{i}" for i in range(60)),
                 )
-                .rename_column("class", "label")
+                .remove_columns(["class"])
                 .train_test_split(
                     test_size=SpliceDataloader.val_size + SpliceDataloader.test_size,
                     shuffle=SpliceDataloader.shuffle,
